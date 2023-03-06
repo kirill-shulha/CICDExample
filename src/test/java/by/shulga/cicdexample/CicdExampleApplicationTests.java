e8b713f781dbb37ca0f24b866ee2d2b6fbb1b1ff
kirill-shulha <strawberrydeejay@gmail.com>
2023-03-06 19:43:18 +0300

    init commit

---
+++ src/test/java/by/shulga/cicdexample/CicdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.shulga.cicdexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
